52e0d6bcccf633053d712fa264955dc72f664574
Martin Llofriu <mllofriu@foccuz.com>
2025-02-26 15:47:02 +0000

    Initial app, receiving messages from twilio through ngrok

---
+++ chicoapp/views.py
@@ -0,0 +1,14 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def webhook(request):
+    if request.method == "POST":
+        data = request.POST
+        parsed_data = {key: data[key] for key in data}
+
+        print(f"Received: {parsed_data}")
+
+        return JsonResponse({"message": "Received", "data": parsed_data}, status=200)
+
+    return JsonResponse({"error": "Only POST requests allowed"}, status=405)
