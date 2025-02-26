151d8b0acb0c93af1f62ce56a2096b623e7ece8e
Martin Llofriu <mllofriu@foccuz.com>
2025-02-26 15:53:28 +0000

    Answer back using twilio's api

--- chicoapp/views.py
+++ chicoapp/views.py
@@ -1,6 +1,8 @@
 from decouple import config
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from twilio.rest import Client
+
 
 @csrf_exempt
 def webhook(request):
@@ -9,6 +11,16 @@ def webhook(request):
         parsed_data = {key: data[key] for key in data}
 
         print(f"Received: {parsed_data}")
+
+        account_sid = config('ACCOUNT_SID')
+        auth_token = config('AUTH_TOKEN')
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+            from_='whatsapp:+14155238886',
+            body=f"I heard: {parsed_data["Body"]}",
+            to=parsed_data["From"]
+        )
 
         return JsonResponse({"message": "Received", "data": parsed_data}, status=200)
 
