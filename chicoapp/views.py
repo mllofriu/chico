e4c5273cac64a313d7dfe697b16abe1efbc91f9f
Martin Llofriu <mllofriu@foccuz.com>
2025-03-05 17:24:32 +0000

    Move to newer langraph api

--- chicoapp/views.py
+++ chicoapp/views.py
@@ -2,9 +2,15 @@ from decouple import config
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from twilio.rest import Client
-from langchain.llms import OpenAI
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph
+from langgraph.graph.message import add_messages
+from typing import Annotated
+from typing_extensions import TypedDict
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
 
 @csrf_exempt
 def webhook(request):
@@ -20,17 +26,27 @@ def webhook(request):
 
         # Forward the message to GPT using LangChain
         openai_api_key = config('OPENAI_API_KEY')
-        llm = OpenAI(api_key=openai_api_key)
-        prompt = PromptTemplate(input_variables=["message"], template="Respond to the following message: {message}")
-        chain = LLMChain(llm=llm, prompt=prompt)
-        gpt_response = chain.run(message=parsed_data["Body"])
+        tools = []
+        llm = ChatOpenAI(api_key=openai_api_key)
 
-        message = client.messages.create(
-            from_='whatsapp:+14155238886',
-            body=f"GPT-3 says: {gpt_response}",
-            to=parsed_data["From"]
-        )
+        def chatbot(state: State):
+            return {"messages": [llm.invoke(state["messages"])]}
+        llm_with_tools = llm.bind_tools(tools)
 
-        return JsonResponse({"message": "Received", "data": parsed_data, "gpt_response": gpt_response}, status=200)
+        graph_builder = StateGraph(State)
+        graph_builder.add_node("chatbot", chatbot)
+        graph_builder.set_entry_point("chatbot")
+        graph_builder.set_finish_point("chatbot")
+        graph = graph_builder.compile()
+
+        for event in graph.stream({"messages": [{"role": "user", "content": parsed_data["Body"]}]}):
+            for value in event.values():
+                gpt_response = value["messages"][-1].content
+                client.messages.create(
+                    from_='whatsapp:+14155238886',
+                    body=f"{gpt_response}",
+                    to=parsed_data["From"]
+                )
+                return JsonResponse({"message": "Received", "data": parsed_data, "gpt_response": gpt_response}, status=200)
 
     return JsonResponse({"error": "Only POST requests allowed"}, status=405)
